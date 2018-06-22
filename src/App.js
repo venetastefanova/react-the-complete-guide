3cffd88a0bc4039c1d25c9f13754ce4655f10097
Veneta Stefanova <venetastefanovaa@gmail.com>
2018-06-22 18:28:08 +0300

    rendering lists

--- src/App.js
+++ src/App.js
@@ -53,19 +53,14 @@ class App extends Component {
     if(this.state.showsPersons){
         persons = (
           <div>
-            <Person 
-                name={this.state.persons[0].name}
-                age={this.state.persons[0].age}
-                click={this.switchNameHandler.bind(this,"veneta stefanova")}/>
-            <Person 
-                name={this.state.persons[1].name}
-                age={this.state.persons[1].age}
-                changed={this.nameChangedHandler}/>
-            <Person 
-                name={this.state.persons[2].name}
-                age={this.state.persons[2].age}
-                click={this.switchNameHandler.bind(this, "Veneta Nikolaeva")}
-                >My hobbies are:</Person>    
+            {
+              this.state.persons.map(person => {
+              return <Person 
+                      name={person.name} 
+                      age={person.name}/>
+              })
+            }
+             
            </div>
 
           
