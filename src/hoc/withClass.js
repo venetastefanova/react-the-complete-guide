e5d57a1171330ab265c51421cb215d8a7116b955
Veneta Stefanova <venetastefanovaa@gmail.com>
2018-06-28 20:22:34 +0300

    passing unknows props to HOC for functional component

--- src/hoc/withClass.js
+++ src/hoc/withClass.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+// you can return functional component or stateful component
 // it's not functional component, it's a normal js function
 // just returns a function that qualifies as functional component
-
 const withClass = (WrappedComponent, className) => {
     return (props)=>(
             <div className={className}>
-                    <WrappedComponent/>
+                {/* never manipulate the WrappedComponent, just use it as it is */}
+                    <WrappedComponent {...props}/> {/* pass the props as you get them*/}
+                {/* {...props} splits the props object into key : value pairs and pass them to WrappedComponent*/}
                 </div>
     )
 };
